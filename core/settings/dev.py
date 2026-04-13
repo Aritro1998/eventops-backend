1315475d837a1e41ddb7db5586c7f8098d828e7c
Aritro Chowdhury <aritro-mac@Mac.Dlink>
2026-04-13 22:34:10 +0530

    Add debug toolbar, setting/dev, urls

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,9 +1,23 @@
-import os
+import os, socket
 
 from .base import *
 
-
 DEBUG = os.getenv("DEBUG", "True").strip().lower() == "true"
 
-# Keep local development permissive by default.
 ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+] + MIDDLEWARE
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "localhost",
+]
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [ip.rsplit(".", 1)[0] + ".1" for ip in ips]
--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -35,3 +36,9 @@ urlpatterns = [
     path('api/bookings/', include(bookings_urls)),
     path('api/workflows/', include(workflows_urls)),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
